b48f5f460aafd353364014932faedf6d12d5d8b1
Thomas Leclercq <thomasleclercq90010@gmail.com>
2017-10-12 16:09:06 +1300

    Dev server running, changes made on images and home for empty db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,7 @@
             </div>
         </div>
 
+        @if(count($products > 0) && count($categories > 0))
         <div id="products" class="container-fluid section">
             <div class="row">
                 <h3>Services</h3>
@@ -79,6 +80,7 @@
                   @endforeach
             </div>
         </div>
+        @endif
 
         <div id="contact" class="section">
             <div class="row">
